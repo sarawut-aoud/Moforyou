f463a712d727b2ba8b23aa903ff557fb6ae47985
sarawut-pcru <sarawut.aou@pcru.com>
2022-01-05 13:46:47 +0700

    json file

--- test.php
+++ test.php
@@ -13,9 +13,8 @@
 require_once './connect/functions.php';
 
 $sql = new mapthailand();
-// $prov = $sql->prov();
-// $amp = $sql->amphur();
-$distall = $sql->distall();
+
+$distall = $sql->amphur();
 // $rs = json_encode($query);
 // district
 // $id= $rs->DISTRICT_ID;
@@ -50,36 +49,30 @@ $distall = $sql->distall();
 
 while ($rs = mysqli_fetch_object($distall)) {
     // prov
-    $p_id = $rs->PROVINCE_ID;
-    $p_code = $rs->PROVINCE_CODE;
-    $p_name = $rs->PROVINCE_NAME;
-    $p_geo_id = $rs->GEO_ID;
-    //amphur  
+    // $p_id = $rs->PROVINCE_ID;
+    // $p_code = $rs->PROVINCE_CODE;
+    // $p_name = $rs->PROVINCE_NAME;
+    // $p_geo_id = $rs->GEO_ID;
+   // amphur  
     $a_id = $rs->AMPHUR_ID;
     $a_code = $rs->AMPHUR_CODE;
     $a_name = $rs->AMPHUR_NAME;
     $zipcode = $rs->POSTCODE;
-    $a_geo_id = $rs->GEO_ID;
-    $a_prov_id = $rs->PROVINCE_ID;
-    // district
-    $d_id = $rs->DISTRICT_ID;
-    $d_code = $rs->DISTRICT_CODE;
-    $d_name = $rs->DISTRICT_NAME;
-    $d_amp_id = $rs->AMPHUR_ID;
-    $d_prov_id = $rs->PROVINCE_ID;
-    $d_geo_id = $rs->GEO_ID;
+    // $a_geo_id = $rs->GEO_ID;
+    // $a_prov_id = $rs->PROVINCE_ID;
+    // // district
+    // $d_id = $rs->DISTRICT_ID;
+    // $d_code = $rs->DISTRICT_CODE;
+    // $d_name = $rs->DISTRICT_NAME;
+    // $d_amp_id = $rs->AMPHUR_ID;
+    // $d_prov_id = $rs->PROVINCE_ID;
+    // $d_geo_id = $rs->GEO_ID;
 
-    $post[] = array(
-        'id' => $d_id, 'id_code' => $d_code, 'name_th' => trim($d_name),
-
-        'amphur'=>[
-            'id' => $a_id, 'id_code' => $a_code, 'name_th' => trim($a_name), 'zipcode' => $zipcode ],
-            'province'=>['id' => $p_id, 'id_code' => $p_code, 'name_th' => trim($p_name)]
-        );
+    $post[] = array('id'=> $a_id, 'id_code' => $a_code, 'name_th' => trim($a_name),'zipcode'=>$zipcode);
     
 }
 
 
-$fp = fopen('prov_amphur_dist.json', 'w');
+$fp = fopen('amphur.json', 'w');
 fwrite($fp, json_encode($post, JSON_PRETTY_PRINT));   // here it will print the array pretty
 fclose($fp);
--- test2.php
+++ test2.php
@@ -1,10 +1,8 @@
 <?php
-     $get_data = file_get_contents('./rs.json');
-    //  @$get_data = file_get_contents('https://raw.githubusercontent.com/kongvut/thai-province-data/master/api_province_with_amphure_tombon.json');
+    // $get_data = file_get_contents('./amphur.json');
+     @$get_data = file_get_contents('https://raw.githubusercontent.com/sarawut-pcru/Thailand_Map/main/json/tombon.json');
     $data = json_decode($get_data);
 
-    echo '<pre>';
-    print_r($data)  ;
-     echo '</pre>';
+    echo $get_data;
 
 ?>
