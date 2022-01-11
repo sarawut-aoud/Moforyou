a6e9d9bb694daa4ade3cd002d26972aeac8880bb
sarawut-pcru <sarawut.aou@pcru.com>
2022-01-11 15:37:41 +0700

    loop api

--- create_json.php
+++ create_json.php
@@ -7,38 +7,39 @@ $sql = new mapthailand();
 
 
 
-$i = 1;
+$i = 0;
 $j = 1;
-
-
 do {
-  
-  $p_id = $data[$i-1]->id;
-  $p_code = $data[$i-1]->id_code;
-  $p_name = $data[$i-1]->name_th;
-  // echo $p_id;
-  
-  $dis = $sql->amphur($j);
-  $rs = mysqli_fetch_object($dis);
-  $apid = $rs->PROVINCE_ID;
-  echo $apid;
- do {
-  
-    if($apid == $p_id){
+ 
+  $p_id = $data[$i]->id;
+  echo $p_id. ": data <br>";
+  $p_code = $data[$i]->id_code;
+  $p_name = $data[$i]->name_th;
+ 
+ 
+  do {
+
+    // echo $p_id;
+    echo $j . ": sql <br>";
+    $dis = $sql->amphur($j);
+    while ($rs = mysqli_fetch_object($dis)) {
       $a_id = $rs->AMPHUR_ID;
       $a_code = $rs->AMPHUR_CODE;
       $a_name = $rs->AMPHUR_NAME;
       $zipcode = $rs->POSTCODE;
-      // echo $a_id."<br>";
-      $amp[] = array('id' => $a_id, 'id_code' => $a_code, 'name_th' => trim($a_name), 'zipcode' => $zipcode);
-    }
-   
-  } while ($apid>$p_id);
 
- 
-  $i = $i + 1;
-} while ($i <=77 || $j<=78);
-$post[] = array('id' => $p_id, 'id_code' => $p_code, 'name_th' => trim($p_name), 'amphur' => $amp);
+      if ($rs->PROVINCE_ID  ==  $p_id) {
+        $amp[] = array('id' => $a_id, 'id_code' => $a_code, 'name_th' => trim($a_name), 'zipcode' => $zipcode);
+      }else{
+        $j=$j+1;
+      }
+      
+    }   
+  } while ($j< $p_id);
+  $i=$i + 1;
+  $post[] = array('id' => $p_id, 'id_code' => $p_code, 'name_th' => trim($p_name), 'amphur' => $amp);
+} while ($i <=76 );
+
 
 $filename = './json/amphur.json';
 $fp = fopen($filename, 'w');
--- test2.php
+++ test2.php
@@ -1,19 +1,33 @@
 <?php
-     $get_data = file_get_contents('./json/amphur.json');
-    //  @$get_data = file_get_contents('https://raw.githubusercontent.com/sarawut-pcru/Thailand_Map/main/json/tombon.json');
-    $data = json_decode($get_data);
+//  $get_data = file_get_contents('./json/amphur.json');
+// //  @$get_data = file_get_contents('https://raw.githubusercontent.com/sarawut-pcru/Thailand_Map/main/json/tombon.json');
+// $data = json_decode($get_data);
 
-  
 
-    echo "<pre>";
-    print_r($data);
-    echo "</pre>";
-    
-  
-  
 
-  
-  
+// echo "<pre>";
+// print_r($data);
+// echo "</pre>";
 
-  
+
+
+for ($i = 1; $i <= 77; $i++) {
+
+    echo "<p> data :" . $i."<br>"  ; //! จังหวัด
+
+    for ($j = 1; $j <= $i; $j++) {
+        echo " sql : " . $j ."<br>"; //? อำเภอ
+        
+        for($x = 1; $x <= $j ; $x++){
+            echo " sql->tombon : " . $x ."<br>"; //todo ตำบล
+        
+        }
+       
+    }
+
+}
 ?>
+
+
+
+
