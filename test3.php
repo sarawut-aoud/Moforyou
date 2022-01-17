00688c7fea63750a7ae6d5286897dfbbb4b1ef3a
sarawut-pcru <sarawut.aou@pcru.com>
2022-01-17 15:00:15 +0700

    update new api error

--- test3.php
+++ test3.php
@@ -1,5 +1,5 @@
 <?php
- $get_data = file_get_contents('./json/amphur.json');
+ $get_data = file_get_contents('./json/all/all.json');
 //  @$get_data = file_get_contents('https://raw.githubusercontent.com/sarawut-pcru/Thailand_Map/main/json/tombon.json');
 $data = json_decode($get_data);
 
