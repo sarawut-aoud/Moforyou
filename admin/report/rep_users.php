db43ec8a1f7005dca6f42af20e1267aeb75ff56a
pocky-tanakorn <lucifuxx@gmail.com>
2022-03-29 23:02:27 +0700

    pock test

--- admin/report/rep_users.php
+++ admin/report/rep_users.php
@@ -84,16 +84,7 @@
                                                
                                             </tr>
                                         </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <th>Rendering engine</th>
-                                                <th>Browser</th>
-                                                <th>Platform(s)</th>
-                                                <th>Engine version</th>
-                                                <th>CSS grade</th>
-                                               
-                                            </tr>
-                                        </tfoot>
+                                       
                                     </table>
                                 </div>
                                 <!-- /.card-body -->
