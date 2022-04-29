95827266321695e7309b8f147cf542cd58ea2fb8
sarawut-pcru <sarawut.aou@pcru.ac.th>
2022-04-29 18:28:14 +0700

    save

--- admin/sub/fooster.php
+++ admin/sub/fooster.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-      <strong>Copyright &copy; 2021 <a href="#">Admin|MFU</a>.</strong>
+      <strong>Copyright &copy; 2021 <a href="#">นักศึกษามหาวิทยาลัยราชภัฏเพชรบูรณ์</a>.</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0.0
