2a021833759fc870dd457e4b0bfed891d1f2f9d7
sarawut-pcru <sarawut.aou@pcru.ac.th>
2022-05-09 12:38:19 +0700

    save

--- connect/database.php
+++ connect/database.php
@@ -1,9 +1,14 @@
 <?php
 error_reporting(~E_NOTICE);
-define('DB_SERVER', 'localhost'); // Hostname
-define('DB_USER', 'root'); //Database Username
-define('DB_PASS', ''); // Database Password
-define('DB_NAME', 'db_moforyou'); // Database Name
+// define('DB_SERVER', 'localhost'); // Hostname
+// define('DB_USER', 'root'); //Database Username
+// define('DB_PASS', ''); // Database Password
+// define('DB_NAME', 'db_moforyou'); // Database Name
+// date_default_timezone_set('Asia/Bangkok');
+define('DB_SERVER', 'computer.pcru.ac.th'); // Hostname
+define('DB_USER', 's611102064109'); //Database Username
+define('DB_PASS', '611102064109'); // Database Password
+define('DB_NAME', 'db611102064109'); // Database Name
 date_default_timezone_set('Asia/Bangkok');
 class Database
 {
